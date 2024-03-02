b6d47294dbf34ec841dfcbeba9aaa2afd37ba760
nagymathev <nagymathev@gmail.com>
2024-03-02 18:39:08 +0100

    no newline in string :( it breaks stuff

--- src/models/index.ts
+++ src/models/index.ts
@@ -3,9 +3,7 @@ import { Sequelize } from "sequelize";
 import getCameraModel from "./camera";
 import getCameraMountModel from "./camera_mount";
 
-const sequelize = new Sequelize(`
-    postgres://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}
-    @${process.env.DATABASE_URL}:${process.env.DATABASE_PORT}/${process.env.DATABASE}`)
+const sequelize = new Sequelize(`postgres://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_URL}:${process.env.DATABASE_PORT}/${process.env.DATABASE}`)
 
 const models = {
     Camera: getCameraModel(sequelize),
